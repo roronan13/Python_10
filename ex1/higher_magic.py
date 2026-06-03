821199d84a860d8cf942c74915f476b98b85f22b
roronan13 <ronan.pothier@gmail.com>
2026-06-03 19:25:40 +0200

    oui

--- ex1/higher_magic.py
+++ ex1/higher_magic.py
@@ -11,16 +11,34 @@ def spell_combiner(spell1: Callable, spell2: Callable) -> Callable:
 
 def power_amplifier(base_spell: Callable, multiplier: int) -> Callable:
     def amplifier_function(target: str, power: int) -> str:
-        print(f"Original power was {power}.")
+        print(f"\nOriginal power was {power}.")
         return (base_spell(target, power * multiplier))
 
     return amplifier_function
 
 
 def conditional_caster(condition: Callable, spell: Callable) -> Callable:
-    def conditional_function(target: str, power: int) -> 
+    def conditional_function(target: str, power: int) -> str:
+        if condition(target, power):
+            print(f"\nPower is {power}.")
+            return (spell(target, power))
+        else:
+            print(f"\nPower is {power}.")
+            return ("Spell fizzled.")
 
-# def spell_sequence(spells: list[Callable]) -> Callable:
+    return conditional_function
+
+
+def spell_sequence(spells: list[Callable]) -> Callable:
+    def sequence_function(target: str, power: int) -> list[str]:
+        print(f"\nSpells are {spells}.")
+        actions_list: list[str] = []
+        for spell in spells:
+            actions_list.append(spell(target, power))
+
+        return actions_list
+
+    return sequence_function
 
 
 if __name__ == "__main__":
@@ -37,8 +55,24 @@ if __name__ == "__main__":
     def explore(target: str, power: int) -> str:
         return (f"Explore gives +{power} vision to {target}.")
 
+    def condition_function(target: str, power: int) -> bool:
+        if power >= 20:
+            return True
+        return False
+
     combiner_function: Callable = spell_combiner(heal, attack)
-    print(f"{combiner_function(random.choice(test_targets), random.choice(test_values))}")
+    print(f"{combiner_function(random.choice(test_targets),
+                               random.choice(test_values))}")
 
     amplifier_function: Callable = power_amplifier(explore, 4)
-    print(f"{amplifier_function(random.choice(test_targets), random.choice(test_values))}")
+    print(f"{amplifier_function(random.choice(test_targets),
+                                random.choice(test_values))}")
+
+    conditional_function: Callable = conditional_caster(condition_function,
+                                                        attack)
+    print(f"{conditional_function(random.choice(test_targets),
+                                  random.choice(test_values))}")
+
+    sequence_function: Callable = spell_sequence([heal, attack, explore])
+    print(f"{sequence_function(random.choice(test_targets),
+                               random.choice(test_values))}")
