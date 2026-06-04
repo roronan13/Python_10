645b795055c12b4bbe35db9af26be3db920fabfd
roronan13 <ronan.pothier@gmail.com>
2026-06-04 12:40:11 +0200

    oui

---
+++ ex3/functools_artifacts.py
@@ -0,0 +1,16 @@
+from collections.abc import Callable
+from typing import Any
+
+
+def spell_reducer(spells: list[int], operation: str) -> int:
+
+
+def partial_enchanter(base_enchantment: Callable) -> dict[str, Callable]:
+
+
+def memoized_fibonacci(n: int) -> int:
+
+
+def spell_dispatcher() -> Callable[[Any], str]:
+
+    
