7106a81ae2880b4382022b92e4053879b66cb181
roronan13 <ronan.pothier@gmail.com>
2026-06-04 17:10:21 +0200

    bon

--- ex4/decorator_mastery.py
+++ ex4/decorator_mastery.py
@@ -1,4 +1,22 @@
+from functools import wraps
+from collections.abc import Callable
 
+
+def spell_timer(func: Callable) -> Callable:
+
+
+def power_validator(min_power: int) -> Callable:
+
+
+def retry_spell(max_attempts: int) -> Callable:
+
+
+class MageGuild:
+    @staticmethod
+    def validate_mage_name(name: str) -> bool:
+
+
+    def cast_spell(self, spell_name: str, power: int) -> str:
 
 
 if __name__ == "__main__":
